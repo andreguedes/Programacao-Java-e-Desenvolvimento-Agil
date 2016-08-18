45fd314fa3ee41017d244ac318bd53da12e41920
andresguedes <andresguedes@live.com>
2016-08-17 23:38:10 -0300

    Aula 1 - Classes em Java

---
+++ Carros/src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Carro c1 = new Carro();
+		c1.potencia = 10;
+		c1.nome = "Corcel";
+		c1.velocidade = 0;
+		
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.frear();
+		
+		c1.imprimir();
+	}
+}
